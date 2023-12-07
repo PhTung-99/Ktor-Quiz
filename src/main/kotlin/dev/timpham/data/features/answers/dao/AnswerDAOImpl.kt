2915ac7e70b3f36e9cdc26c3242587c3f3bf32e1
Tung Pham <phtung.99@gmail.com>
2023-12-07 20:20:35 +0700

    update

---
+++ src/main/kotlin/dev/timpham/data/features/answers/dao/AnswerDAOImpl.kt
@@ -0,0 +1,45 @@
+package dev.timpham.data.features.answers.dao
+
+import dev.timpham.data.database.DatabaseFactory.dbQuery
+import dev.timpham.data.features.answers.entity.AnswerEntity
+import dev.timpham.data.features.answers.entity.Answers
+import dev.timpham.data.features.answers.mapper.entityToAnswer
+import dev.timpham.data.features.answers.models.Answer
+import dev.timpham.data.features.question.entity.QuestionEntity
+import java.util.UUID
+
+class AnswerDAOImpl: AnswerDAO {
+    override suspend fun getAnswerById(id: UUID): Answer? = dbQuery {
+        AnswerEntity.findById(id)?.let(::entityToAnswer)
+    }
+
+    override suspend fun getAnswersByQuestionId(questionId: UUID): List<Answer> = dbQuery {
+        AnswerEntity.find {
+            Answers.question eq questionId
+        }.map(::entityToAnswer)
+    }
+
+    override suspend fun createAnswer(content: String, isCorrect: Boolean, questionId: UUID): Answer = dbQuery {
+        AnswerEntity.new {
+            this.content = content
+            this.isCorrect = isCorrect
+            this.question = QuestionEntity[questionId]
+        }.let(::entityToAnswer)
+    }
+
+    override suspend fun updateAnswer(id: UUID, content: String, isCorrect: Boolean, questionId: UUID): Answer? = dbQuery {
+        val answer = AnswerEntity.findById(id)
+        answer?.apply {
+            this.content = content
+            this.isCorrect = isCorrect
+            this.question = QuestionEntity[questionId]
+        }?.let(::entityToAnswer)
+    }
+
+    override suspend fun deleteAnswer(id: UUID): Boolean = dbQuery {
+        AnswerEntity.findById(id)?.let{
+            it.delete()
+            return@dbQuery  true
+        }?: false
+    }
+}
